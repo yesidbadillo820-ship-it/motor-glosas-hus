18c237b6b757462c49a89cd25b12595ec5068ad5
yesidbadillo820-ship-it <yesidbadillo820@gmail.com>
2026-03-27 08:28:28 -0500

    database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./glosas.db"
 
@@ -11,6 +10,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
