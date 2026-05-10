773365c317bc2e2a9cf349c0acb67dad39665580
yesidbadillo820-ship-it <yesidbadillo820@gmail.com>
2026-05-09 19:27:45 -0500

    stub: exportar_gerencial

---
+++ app/services/exportar_gerencial.py
@@ -0,0 +1,6 @@
+"""DEPRECATED: exportar gerencial removido en cleanup mayo 2026. Stub."""
+import io
+
+
+def generar_xlsx_gerencial(*args, **kwargs) -> bytes:
+    return io.BytesIO(b"REMOVED").getvalue()
