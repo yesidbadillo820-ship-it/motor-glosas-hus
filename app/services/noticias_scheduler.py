766232e76a138a0fd960915fb83d6bcffb013c54
yesidbadillo820-ship-it <yesidbadillo820@gmail.com>
2026-05-09 19:27:24 -0500

    stub: noticias_scheduler

---
+++ app/services/noticias_scheduler.py
@@ -0,0 +1,15 @@
+"""DEPRECATED: noticias scheduler removido en cleanup mayo 2026. Stub."""
+
+_task = None
+
+
+def iniciar_scheduler():
+    pass
+
+
+def detener_scheduler():
+    pass
+
+
+def obtener_estado() -> dict:
+    return {"activo": False, "removido": True}
